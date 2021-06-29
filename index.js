e06e08415cda4cbb6e9472fa3f37233d3c722601
Joe Christensen <joechristensenwork@gmail.com>
2021-06-29 08:43:27 -0700

    commit tree d43a45796fcfac36d61724a09f3b3c2c99ee9f0b

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+const findMatching = (nameArray, name) => {
+    return nameArray.filter((element) => {return element.toLowerCase() === name.toLowerCase()});
+}
+
+const fuzzyMatch = (nameArray, name) => {
+    return nameArray.filter((element) => {
+        if(element.slice(0, name.length).toLowerCase() === name.toLowerCase()) {
+            return true;
+        }
+        else {
+            return false;
+        }});
+}
+
+const matchName = (driverArray, name) => {
+    return driverArray.filter((element) => {
+        let elementName = element.name;
+        console.log(elementName);
+        return elementName.toLowerCase() === name.toLowerCase();
+    });
+}
